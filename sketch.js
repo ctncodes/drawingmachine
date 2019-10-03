5f6972b3ca2b7c266327bf4452c00cd37c2a0cd4
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-03 16:23:49 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,47 @@
 function setup(){
+  //Drawing #1
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #2
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #3
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #4
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #5
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #6
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #7
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #8
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #9
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #10
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #11
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #12
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #13
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #14
+  createCanvas(600,600);
+  background(250,235,215);
+  //Drawing #15
   createCanvas(600,600);
   background(250,235,215);
   strokeWeight(5);
