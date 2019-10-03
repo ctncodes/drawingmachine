db506c247a869546859d541acef4ac21a5468297
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-03 16:43:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,48 +2,48 @@ function setup(){
   //Drawing #1
   createCanvas(450,450);
   background(250,235,215);
-  //Drawing #2
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #3
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #4
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #5
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #6
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #7
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #8
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #9
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #10
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #11
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #12
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #13
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #14
-  createCanvas(450,450);
-  background(250,235,215);
-  //Drawing #15
-  createCanvas(450,450);
-  background(250,235,215);
+  // //Drawing #2
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #3
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #4
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #5
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #6
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #7
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #8
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #9
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #10
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #11
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #12
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #13
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #14
+  // createCanvas(450,450);
+  // background(250,235,215);
+  // //Drawing #15
+  // createCanvas(450,450);
+  // background(250,235,215);
   strokeWeight(5);
 }
 
