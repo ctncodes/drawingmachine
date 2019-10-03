9345885a66ece1e6747a9aec57d41489f8d25d72
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-03 16:36:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,48 @@
 function setup(){
   //Drawing #1
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #2
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #3
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #4
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #5
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #6
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #7
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #8
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #9
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #10
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #11
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #12
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #13
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #14
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   //Drawing #15
-  createCanvas(600,600);
+  createCanvas(450,450);
   background(250,235,215);
   strokeWeight(5);
 }
@@ -50,7 +50,7 @@ function setup(){
 function draw(){
 
 if (mouseIsPressed){
-  stroke(map(mouseX, 0, 600, 0, true));
+  stroke(map(mouseX, 0, 450, 0, true));
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
 }
