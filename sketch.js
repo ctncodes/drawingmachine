3f170c661eb2f842f2e45e22e3fd19014ce68bf1
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-03 16:07:48 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 function setup(){
   createCanvas(600,600);
   background(250,235,215);
-  strokeWeight(23);
+  strokeWeight(5);
 }
 
 function draw(){
 
 if (mouseIsPressed){
   stroke(map(mouseX, 0, 600, 0, true));
-  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
 }
