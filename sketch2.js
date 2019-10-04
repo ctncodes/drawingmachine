e42e107429e8b9674409234d5cee8a5b553f9d52
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-03 17:43:33 -0700

    Recreating The Grid from TRON: Legacy

--- sketch2.js
+++ sketch2.js
@@ -16,12 +16,12 @@ let sketch2 = function(p) {
   };
 };
 let myp6 = new p5(sketch2);
-// let sketch2 = function(p) {
-//   p.setup = function() {
-//     p.createCanvas(450, 450);
-//   };
-//   p.draw = function() {
-//     p.background(104);
-//   };
-// };
-// let myp7 = new p5(sketch2);
+let sketch3 = function(p) {
+  p.setup = function() {
+    p.createCanvas(450, 450);
+  };
+  p.draw = function() {
+    p.background(104);
+  };
+};
+let myp7 = new p5(sketch3);
