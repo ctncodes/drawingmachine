6fa6291e9e2901a7d5fa4a8600ac79f9ca7a06dc
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-05 23:04:17 -0700

    Making some noise

--- sketch2.js
+++ sketch2.js
@@ -21,7 +21,115 @@ let sketch3 = function(p) {
     p.createCanvas(450, 450);
   };
   p.draw = function() {
-    p.background(104);
+    p.background(102);
   };
 };
 let myp7 = new p5(sketch3);
+let sketch4 = function(p) {
+  p.setup = function() {
+    p.createCanvas(450, 450);
+  };
+  p.draw = function() {
+    p.background(153);
+  };
+};
+let myp8 = new p5(sketch4);
+let sketch5 = function(p) {
+  p.setup = function() {
+    p.createCanvas(450, 450);
+  };
+  p.draw = function() {
+    p.background(204);
+  };
+};
+let myp9 = new p5(sketch5);
+// let sketch6 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(0);
+//   };
+// };
+// let myp10 = new p5(sketch6);
+// let sketch7 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(51);
+//   };
+// };
+// let myp11 = new p5(sketch7);
+// let sketch12 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(102);
+//   };
+// };
+// let myp12 = new p5(sketch8);
+// let sketch9 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(153);
+//   };
+// };
+// let myp8 = new p5(sketch4);
+// let sketch5 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(204);
+//   };
+// };
+// let myp9 = new p5(sketch5);
+// let sketch = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(0);
+//   };
+// };
+// let myp5 = new p5(sketch);
+// let sketch2 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(51);
+//   };
+// };
+// let myp6 = new p5(sketch2);
+// let sketch3 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(102);
+//   };
+// };
+// let myp7 = new p5(sketch3);
+// let sketch4 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(153);
+//   };
+// };
+// let myp8 = new p5(sketch4);
+// let sketch5 = function(p) {
+//   p.setup = function() {
+//     p.createCanvas(450, 450);
+//   };
+//   p.draw = function() {
+//     p.background(204);
+//   };
+// };
+// let myp9 = new p5(sketch5);
