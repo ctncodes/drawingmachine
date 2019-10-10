15ae10062b2a24445a75a3cbf23362c77740c0d8
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-10 09:53:21 -0700

    Update sketch5.js

--- sketch5.js
+++ sketch5.js
@@ -9,18 +9,18 @@ function setup() {
   drawChessBoard();
   noFill();
 }
-// function draw() {
-//   if (mouseIsPressed) {
-//     checkeredPattern += 5;
-//     background(checkeredPattern);
-//     array.push([mouseX,mouseY]);
-//     beginShape();
-//     for (let i = 0; i < array.length; i++) {
-//       curveVertex(array[i][0],array[i][1])
-//     }
-//     endShape();
-//   }
-// }
+function draw() {
+  if (mouseIsPressed) {
+    checkeredPattern += 5;
+    background(checkeredPattern);
+    array.push([mouseX,mouseY]);
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      curveVertex(array[i][0],array[i][1])
+    }
+    endShape();
+  }
+}
 function keyTyped() {
   if (key === 's') {
     saveCanvas('fileName', 'png');
