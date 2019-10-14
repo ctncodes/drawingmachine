0081deafcce056ef009c185eecaf035e304a18fb
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-14 16:08:47 -0700

    Returning to the Basics

--- sketch2.js
+++ sketch2.js
@@ -7,14 +7,12 @@ let sketch = function(p) {
     p.checkeredBlue = 0;
     p.theGrid = [];
     p.square1 = p.loadImage('assets/Leonardo_da_Vinci - Self-Portrait.jpg');
-    // p.square1 = p.loadImage('assets/Sora in The Grid.png');
     p.createCanvas(400, 400);
     // p.drawChessBoard();
     // p.noFill();
+    p.background(250, 235, 215);
   };
   p.draw = function() {
-    p.background(250, 235, 215);
-    // p.background(10);
     p.background(p.square1);
     // p.background('#453B32');
     // if (p.mouseIsPressed) {
@@ -31,7 +29,7 @@ let sketch = function(p) {
     // }
   };
   p.keyTyped = function() {
-    if (p.key === 's') {
+    if (p.key === 'a') {
       p.saveCanvas('fileName', 'png');
     } else if (key === 'd') {
       p.background(255);
@@ -80,20 +78,25 @@ let sketch = function(p) {
 };
 let myp5 = new p5(sketch);
 let sketch2 = function(p) {
-
   p.setup = function() {
-  // p.square1 = p.loadImage('assets/Sora in The Grid.png');
     p.createCanvas(400, 400);
-  };
-  p.draw = function() {
     // p.background('#453B32');
     p.background(255, 238, 195);
-    p.background(p.square1);
+    p.strokeWeight(5);
   };
+  p.draw = function() {
+    if (p.mouseIsPressed) {
+      p.line(p.mouseX,p.mouseY,p.pmouseX,p.pmouseY);
+    }
+  };
+  p.keyTyped = function() {
+    if (p.key === 'b') {
+      p.saveCanvas('fileName', 'png');
+    }
+  }
 };
 let myp6 = new p5(sketch2);
 let sketch3 = function(p) {
-
   p.setup = function() {
     p.square1 = p.loadImage('assets/Mona Lisa.jpg');
     p.createCanvas(400, 400);
@@ -106,14 +109,22 @@ let sketch3 = function(p) {
 };
 let myp7 = new p5(sketch3);
 let sketch4 = function(p) {
-
   p.setup = function() {
     p.createCanvas(400, 400);
-  };
-  p.draw = function() {
     // p.background('#453B32');
     p.background(236, 220, 207);
+    p.strokeWeight(5);
   };
+  p.draw = function() {
+    if (p.mouseIsPressed) {
+      p.line(p.mouseX,p.mouseY,p.pmouseX,p.pmouseY);
+    }
+  };
+  p.keyTyped = function() {
+    if (p.key === 'c') {
+      p.saveCanvas('fileName', 'png');
+    }
+  }
 };
 let myp8 = new p5(sketch4);
 let sketch5 = function(p) {
